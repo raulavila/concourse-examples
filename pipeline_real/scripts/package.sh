226d18249cb56052c899dae6cb08d0a79b66d161
RaulAvila <ravila80@gmail.com>
2016-06-20 22:32:15 +0100

    Fail fast on maven package

--- pipeline_real/scripts/package.sh
+++ pipeline_real/scripts/package.sh
@@ -3,5 +3,9 @@
 cd source-code
 mvn clean package -DskipTests
 
+if [ $? -ne 0 ]; then
+  exit 1
+fi
+
 cp manifest.yml ../cf-deploy-files
 cp target/hello-world-0.0.1-SNAPSHOT.jar ../cf-deploy-files
